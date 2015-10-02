339f4ff9c5b09b20c466b7be8f24378c2973c33d
Blubberboy333 <thecowmooman@gmail.com>
2015-10-02 18:28:59 -0400

    Update Main.php

--- src/JoinPopups/Main.php
+++ src/JoinPopups/Main.php
@@ -1,1 +1,63 @@
+<?php
 
+namespace JoinPopups;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\utils\Config;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerQuitEvent;
+
+class Main extends PluginBase implements Listener{
+    public function onEnable(){
+        $this->saveDefaultConfig();
+        $this->playersEnabled = array();
+        $this->playersOnline = count($this->getServer()->getOnlinePlayers());
+        $this->getLogger()->info("§aJoinPopups enabled");
+    }
+    public function onDisable(){
+        $this->getLogger()->sendMessage("§4JoinPopups disabled");
+    }
+    
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+        if(strtolower($command->getName()) == "popup"){
+            if($sender->hasPermission("joinpopup") || $sender->hasPermission("joinpopup.command")){
+                if(isset($args[0]) == "off"){
+                    if(isset($this->playersEnabled[$sender->getName()])){
+                        unset($this->playersEnabled[$sender->getName()]);
+                        $sender->sendMessage("Popups disabled");
+                        return true;
+                    }else{
+                        $sender->sendMessage("Popups aren't enabled for you");
+                        return true;
+                    }
+                }elseif($args[0] == "on"){
+                    if(isset($this->playersEnabled[$sender->getName()])){
+                        $sender->sendMessage("Popups are already enabled for you");
+                        return true;
+                    }else{
+                        $this->playersEnabled[$sender->getName] = $sender->getName();
+                        $sender->sendMessage("Popups has been enabled for you");
+                        return true;
+                    }
+                }else{
+                    $sender->sendMessage("Unknown argument: ".$args[0]);
+                    return true;
+                }
+            }else{
+                $sender->sendMessage("You don't have permission to use that command!");
+                return true;
+            }
+        }
+    }
+    
+    public function onJoin(PlayerJoinEvent $event){
+        $this-playersEnabled[$event->getPlayer()->getName()] = $event->getPlayer()->getName();
+    }
+    
+    public function onLeave(PlayerQuitEvent $event){
+        if(isset($this->playersEnabled[$event->getPlayer()->getName()])){
+            unset($this->playersEnabled[$event->getPlayer()->getName()]);
+        }
+    }
+}
